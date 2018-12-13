53bd8faadda803bcd0f62d410001e96fb22b4e94
harrisonchan9 <44187324+harrisonchan9@users.noreply.github.com>
2018-12-13 17:06:06 +0800

    clean up

--- src/pages/page_home.jsx
+++ src/pages/page_home.jsx
@@ -209,9 +209,6 @@ class PureHomePage extends React.Component {
 					>
 						<div style={{ height: "1px", opacity: "0" }} />
 					</VisibilitySensor> */}
-					<button onClick={this.changeHotel}>0</button>
-					<button onClick={this.changeHotel}>2</button>
-					<div onClick={this.autoCopy}>asdfgr#1234</div>
 					{/* <button onClick={this.changeHotel}>81</button>
 					<button onClick={this.changeHotel}>375</button>
 					<button onClick={this.changeHotel}>1357</button> */}
